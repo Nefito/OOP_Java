e23b0eaf1ca2438d833ee3889fc8c1484554d44c
Nefito <43294012+Nefito@users.noreply.github.com>
2019-03-05 10:03:14 +0200

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,16 @@
+package ua.lpnuai.oop.korzhov1;
+
+import java.util.Scanner;
+
+public class Menu {
+	public static String input() {
+		System.out.println("Enter your input:");
+		Scanner string = new Scanner(System.in);
+		String str = string.nextLine();
+		
+		return str;
+	}
+	
+	
+
+}
